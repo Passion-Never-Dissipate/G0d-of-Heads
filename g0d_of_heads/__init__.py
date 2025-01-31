55c938b89d95592ff80832d9791a06cbad9ba967
FRUITS_CANDY <84139787+FRUITS-CANDY@users.noreply.github.com>
2025-01-31 19:55:53 +0800

    Add files via upload

---
+++ g0d_of_heads/__init__.py
@@ -0,0 +1,29 @@
+from mcdreforged.api.all import *
+
+
+def on_load(server: PluginServerInterface, old):
+    server.logger.info('正在注册指令')
+    server.register_help_message('!!head <player> [个数]', '获取指定玩家的头颅,支持同时获取多个。')
+    builder = SimpleCommandBuilder()
+    builder.command("!!head <player>", give_heads)
+    builder.command("!!head <player> <count>", give_heads)
+    builder.arg("player", Text)
+    builder.arg("count", Integer)
+    builder.register(server)
+    
+
+def give_heads(source: InfoCommandSource, dic: dict):
+    if not source.is_player:
+        source.reply("§c§l只有玩家才能使用该插件！！")
+        return
+
+    if "count" in dic and int(dic["count"]) < 1:
+        source.reply("§c§l头颅数量应为大于0的整数")
+        return
+
+    owner = source.get_info().player
+    player = dic["player"]
+    source.get_server().execute(
+        f'execute at {owner} run give {owner} minecraft:player_head{{SkullOwner:"{player}"}} {1 if "count" not in dic else dic["count"]}'
+    )
+    source.reply(f"§bGOH: {owner} 获取了 {player} 的头颅")
